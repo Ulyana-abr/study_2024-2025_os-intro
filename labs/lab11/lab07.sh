5e31f84172bcad939307af07b80164f5d11f1884
Ulyana Abramova <abramovaym@bk.ru>
2025-04-25 23:37:53 +0300

    feat(main):make course structure

---
+++ labs/lab11/lab07.sh
@@ -0,0 +1,1 @@
+#!/bin/bash,HELL=Hellofunction hello {    LOCAL  HELLO=World    echo $HELLO}helloecho $HELLO#!/bin/bash,HELL=Hellofunction hello {    LOCAL  HELLO=World        echo $HELLO}helloec summ
